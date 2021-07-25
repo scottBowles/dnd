1230be399cdf5082d934c104fe19f4c92b025031
scottBowles <shbowles@gmail.com>
2021-07-24 22:50:45 -0400

    Configure postgresql db

--- api/website/settings.py
+++ api/website/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "website.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "dnd",
+        "USER": "scottbowles",
+        "PASSWORD": "fJx*19B2gtV1lKVK",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
