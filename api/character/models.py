1ef8e37bccc1f598d7655c6b70dcec9978255218
scottBowles <shbowles@gmail.com>
2021-07-25 00:02:54 -0400

    Add character classes & hook up through drf

--- api/character/models.py
+++ api/character/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
-# Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=200)
+    character_class = models.CharField(max_length=200)
+    hit_points = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class CharacterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Character
---
+++ api/character/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Character
+
+
+class CharacterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Character
