9d50bf702cf07259670b7df45f48fa272ac5ce09
scottBowles <shbowles@gmail.com>
2021-08-11 19:19:27 -0400

    Make DEFAULT_AUTO_FIELD BigAutoField with migration

--- api/website/settings.py
+++ api/website/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
