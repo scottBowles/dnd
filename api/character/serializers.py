ad6ff8a65e70453152bf006c8bed07e76d99b3e4
scottBowles <shbowles@gmail.com>
2021-08-11 19:51:49 -0400

    Change serializer to show user

--- api/character/serializers.py
+++ api/character/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Character
 
 
-class CharacterSerializer(serializers.HyperlinkedModelSerializer):
+class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
-        exclude = ("user",)
+        fields = "__all__"
