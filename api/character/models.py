11f3c3718cc76bd13b15349b41a97a4cc989d573
scottBowles <shbowles@gmail.com>
2021-08-11 22:02:51 -0400

    Add alignment choices

--- api/character/models.py
+++ api/character/models.py
@@ -3,13 +3,26 @@ from django.contrib.auth.models import User
 
 
 class Character(models.Model):
+    class Alignment(models.TextChoices):
+        LAWFUL_GOOD = "LG", "Lawful Good"
+        NEUTRAL_GOOD = "NG", "Neutral Good"
+        CHAOTIC_GOOD = "CG", "Chaotic Good"
+        LAWFUL_NEUTRAL = "LN", "Lawful Neutral"
+        TRUE_NEUTRAL = "N", "True Neutral"
+        CHAOTIC_NEUTRAL = "CN", "Chaotic Neutral"
+        LAWFUL_EVIL = "LE", "Lawful Evil"
+        NEUTRAL_EVIL = "NE", "Neutral Evil"
+        CHAOTIC_EVIL = "CE", "Chaotic Evil"
+
     name = models.CharField(max_length=200, null=True, blank=True)
+    alignment = models.CharField(
+        max_length=2, choices=Alignment.choices, null=True, blank=True
+    )
     character_class = models.CharField(
         max_length=200, null=True, blank=True
     )  # many to many? include levels?
     level = models.IntegerField(default=1)  # per character class?
     background = models.CharField(max_length=200, null=True, blank=True)  # fk
-    alignment = models.CharField(max_length=200, null=True, blank=True)  # fk
     race = models.CharField(max_length=200, null=True, blank=True)  # fk
     experience_points = models.IntegerField(default=1)
     hit_points = models.IntegerField(default=0)  # calculated? probably not
