0001d9eb7be03ba75a2cd395b64273cb2b54b737
scottBowles <shbowles@gmail.com>
2022-02-06 02:04:07 -0500

    Add django-filter, atomic mutations

--- website/settings.py
+++ website/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -176,4 +175,5 @@ DJOSER = {
 
 GRAPHENE = {
     "SCHEMA": "website.schema.schema",
+    "ATOMIC_MUTATIONS": True,
 }
