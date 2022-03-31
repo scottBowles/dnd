84040cf66ba7980b20a251fb45385ac0c2bff619
scottBowles <shbowles@gmail.com>
2022-03-30 21:18:04 -0400

    Even more print statements on my poor man's debug middleware

--- website/middleware.py
+++ website/middleware.py
@@ -3,9 +3,15 @@ class PrintRequestsMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        print("request.path")
         print(request.path)
+        print("request.method")
         print(request.method)
+        print("request.body")
         print(request.body)
+        print("request.headers")
+        print(request.headers)
         response = self.get_response(request)
+        print("response.content")
         print(response.content)
         return response
