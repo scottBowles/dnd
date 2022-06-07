0d8858aced9bc337d4184f34a9fcd94cdaad87c7
scottBowles <shbowles@gmail.com>
2022-06-06 20:16:02 -0400

    Try using CompressedStaticFilesStorage

--- website/settings.py
+++ website/settings.py
@@ -171,7 +171,8 @@ STATIC_URL = "/static/"
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
