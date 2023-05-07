07f92cf99ceb9072c4a6bc7d824219499b05dec4
scottBowles <shbowles@gmail.com>
2023-05-07 14:36:46 -0400

    Change build.sh to use pip instead of pipenv

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-pipenv install
+# pipenv install
 
 # pipenv shell
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
