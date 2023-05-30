6ff19dd76b52b596d2180963e1d22ae8e0f24aa3
scottBowles <shbowles@gmail.com>
2023-05-30 18:26:48 -0400

    Fix ai helpers and put in own file

--- nucleus/gdrive.py
+++ nucleus/gdrive.py
@@ -3,7 +3,6 @@ from googleapiclient.discovery import build
 
 GOOGLE_API_KEY = settings.GOOGLE_API_KEY
 AIREL_FOLDER_ID = settings.AIREL_FOLDER_ID
-OPENAI_API_KEY = settings.OPENAI_API_KEY
 
 
 def fetch_airel_folder():
@@ -36,33 +35,3 @@ def fetch_airel_file_text(id):
     with build("drive", "v3", developerKey=GOOGLE_API_KEY) as service:
         results = service.files().export(fileId=id, mimeType="text/plain").execute()
         return results.decode("utf-8")
-
-
-def openai_summarize_text(text):
-    """
-    Summary the given text using openai
-    This is first to be used for summarizing long game logs (~13000 character) into a short summary
-    """
-    import openai
-
-    openai.api_key = OPENAI_API_KEY
-
-    text = (
-        "Summarize the following game log from a game of dungeons and dragons into a few paragraphs:\n\n"
-        + text
-        + "\n\nSummary:"
-    )
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=text,
-        temperature=0.3,
-        max_tokens=60,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        n=1,
-    )
-    return response["choices"][0]["text"]
-
-
