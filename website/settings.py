84c27c9ca755465df1eb4ce38f06a91b0f7f3a42
scottBowles <shbowles@gmail.com>
2025-07-03 18:09:50 -0400

    Comment extra logging in settings.py

--- website/settings.py
+++ website/settings.py
@@ -16,19 +16,19 @@ import dj_database_url
 from dotenv import load_dotenv
 from gqlauth.settings_type import GqlAuthSettings
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
-    },
-}
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "console": {
+#             "class": "logging.StreamHandler",
+#         },
+#     },
+#     "root": {
+#         "handlers": ["console"],
+#         "level": "DEBUG",
+#     },
+# }
 
 
 load_dotenv()
