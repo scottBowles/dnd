9a8da84da7c8c76dd77bfca384346b39cbfb363e
scottBowles <shbowles@gmail.com>
2025-07-19 14:55:46 -0400

    Hopefully fix the character global id for algolia indexing and search

--- character/models/character.py
+++ character/models/character.py
@@ -137,7 +137,7 @@ class Character(Entity):
     size = models.CharField(max_length=10, choices=SIZES, null=True, blank=True)
 
     def global_id(self):
-        return to_global_id("NPC", self.id)
+        return to_global_id("Character", self.id)
 
     def __str__(self):
         return self.name or ""
