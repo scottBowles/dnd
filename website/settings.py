d8cb9e84f4e442a5f4a070d4910c4a2f207c2db1
scottBowles <shbowles@gmail.com>
2025-07-26 18:03:33 -0400

    Add django.contrib.postgres to INSTALLED_APPS

--- website/settings.py
+++ website/settings.py
@@ -96,6 +96,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "corsheaders",
     "strawberry_django",
     "gqlauth",
