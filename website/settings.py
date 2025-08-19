9064a8f27cce8bb4b00d9847a26c813632e86d21
scottBowles <shbowles@gmail.com>
2025-08-18 20:54:58 -0400

    Comment out the excessive logging

--- website/settings.py
+++ website/settings.py
@@ -17,19 +17,19 @@ from dotenv import load_dotenv
 from gqlauth.settings_type import GqlAuthSettings
 import datetime
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
-    },
-}
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "console": {
+#             "class": "logging.StreamHandler",
+#         },
+#     },
+#     "root": {
+#         "handlers": ["console"],
+#         "level": "DEBUG",
+#     },
+# }
 
 
 load_dotenv()
