d956589c36682b66eb599f462b84064b2b252dce
scottBowles <shbowles@gmail.com>
2025-08-28 18:38:54 -0400

    Fix game log fts

--- rag_chat/services/game_log_full_text_search.py
+++ rag_chat/services/game_log_full_text_search.py
@@ -4,9 +4,13 @@ from nucleus.models import GameLog
 
 
 def game_log_fts(query_str, limit=10):
-    query = SearchQuery(query_str, config="simple")
+    # Consider changing config to 'english' if this doesn't work well
+    # - simple should work better for fantasy names and terms
+    # - english will do stemming, stopwords, but may mangle fantasy terms
+    query = SearchQuery(query_str, config="simple", search_type="websearch")
+
     return (
         GameLog.objects.annotate(rank=SearchRank(F("full_text_search_vector"), query))
-        .filter(full_text_search_vector=query)
+        .filter(full_text_search_vector__isnull=False)
         .order_by("-rank")[:limit]
     )
